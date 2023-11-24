1d87fa18ece7b4d2173e7422ad05a18566af168b
vmh0104 <151828214+vmh0104@users.noreply.github.com>
2023-11-24 11:17:55 +0700

    Update app.js

--- app.js
+++ app.js
@@ -40,63 +40,6 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.on('request', (req, res) => {
-  console.log('Received request:', req.url, req.method);
-
-  if (req.url === '/' && req.method === 'GET') {
-      console.log('Handling GET /');
-      return res.end(fs.readFileSync(__dirname + '/index.jade'));
-  }
-
-  if (req.url.startsWith('/upload') && req.method === 'POST') {
-      console.log('Handling POST /upload');
-
-      const queryString = req.url.split('?')[1];
-      const fileName = new URLSearchParams(queryString).get('fileName');
-
-      if (!fileName) {
-          console.error('Bad Request: Missing file name.');
-          res.statusCode = 400;
-          return res.end('Bad Request: Missing file name.');
-      }
-
-      if (!fileName.toLowerCase().endsWith('.csv')) {
-          console.error('Bad Request: File is not a CSV.');
-          res.statusCode = 400;
-          return res.end('Bad Request: File is not a CSV.');
-      }
-
-      const filePath = path.join('E:/App', fileName);
-      console.log('File path:', filePath);
-
-      const writeStream = fs.createWriteStream(filePath, { flags: 'w' });
-
-      req.on('data', (chunk) => {
-          writeStream.write(chunk);
-      });
-
-      req.on('end', () => {
-          writeStream.end();
-
-          writeStream.on('finish', () => {
-              console.log('File write complete.');
-              res.end(`File uploaded: ${fileName}`);
-          });
-      });
-
-      writeStream.on('error', (err) => {
-          console.error('Error writing file:', err);
-          res.statusCode = 500;
-          return res.end('Internal Server Error: Could not write file.');
-      });
-
-      return;
-  }
-
-  console.log('Not Found');
-  res.statusCode = 404;
-  res.end('Not Found');
-});
 
 module.exports = app;
 
