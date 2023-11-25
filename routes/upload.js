aaaa471e6fe9ce9b26620fc15083d157b1a5bee3
vmh0104 <vumanhhieuu.nd@gmail.com>
2023-11-26 02:14:00 +0700

    Your commit message

--- routes/upload.js
+++ routes/upload.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-
-
 router.get('/upload', function(req, res, next) {
       res.render('upload', { title: 'Choose File' });
       console.log('Handling GET /upload');    
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
 router.get('/user', function(req, res, next) {
   res.send('respond with a resource');
 });
