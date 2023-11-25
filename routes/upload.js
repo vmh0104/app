3e55e46862801e376fc8acc9ae85c3a3ec9849a5
vmh0104 <vumanhhieuu.nd@gmail.com>
2023-11-25 21:53:07 +0700

    Your commit message

---
+++ routes/upload.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/upload', function(req, res, next) {
+      res.render('upload', { title: 'Choose File' });    
+  });
+
+
+router.post('/upload', function(req, res, next) {
+    console.log('Handling POST /upload');
+
+    const queryString = req.url.split('?')[1];
+    const fileName = new URLSearchParams(queryString).get('fileName');
+
+    if (!fileName) {
+        console.error('Bad Request: Missing file name.');
+        res.statusCode = 400;
+        return res.end('Bad Request: Missing file name.');
+    }
+
+    if (!fileName.toLowerCase().endsWith('.csv')) {
+        console.error('Bad Request: File is not a CSV.');
+        res.statusCode = 400;
+        return res.end('Bad Request: File is not a CSV.');
+    }
+    console.log(fileName);
+});
+module.exports = router;
