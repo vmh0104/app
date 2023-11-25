32138736aff354a0fbaf7f076f8dc9e333b40365
vmh0104 <vumanhhieuu.nd@gmail.com>
2023-11-25 15:11:39 +0700

    Your commit message

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const fs = require('fs');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+
+
 var app = express();
 
 // view engine setup
@@ -23,6 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -40,10 +44,8 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-<<<<<<< HEAD
 
-=======
->>>>>>> ae35ec2 (Initial commit)
+
 module.exports = app;
 
 
--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,60 @@ router.get('/upload', function(req, res, next) {
   res.render('index', { title: 'Upload File' });
   console.log("User Router Working");
   console.log('Server running on port 3000');
-    
+  
+
 });
+
+router.post('/upload', function(req, res, next){
+    console.log('Received request:', req.url, req.method);
+  if (req.url === '/' && req.method === 'GET') {
+    console.log('Handling GET /');
+    return res.end(fs.readFileSync(__dirname + '/index.jade'));
+  }
+  if (req.url.startsWith('/upload') && req.method === 'POST') {
+    console.log('Handling POST /upload');
+    const queryString = req.url.split('?')[1];
+        const fileName = new URLSearchParams(queryString).get('fileName');
+
+        if (!fileName) {
+            console.error('Bad Request: Missing file name.');
+            res.statusCode = 400;
+            return res.end('Bad Request: Missing file name.');
+        }
+
+        if (!fileName.toLowerCase().endsWith('.csv')) {
+            console.error('Bad Request: File is not a CSV.');
+            res.statusCode = 400;
+            return res.end('Bad Request: File is not a CSV.');
+        }
+        console.log(fileName);
+        // const filePath = path.join(__dirname + fileName);
+        
+
+        // const writeStream = fs.createWriteStream(filePath, { flags: 'w' });
+
+        // req.on('data', (chunk) => {
+        //     writeStream.write(chunk);
+        // });
+
+        // req.on('end', () => {
+        //     writeStream.end();
+
+        //     writeStream.on('finish', () => {
+        //         console.log('File write complete.');
+        //         res.end(`File uploaded: ${fileName}`);
+        //     });
+        // });
+
+        // writeStream.on('error', (err) => {
+        //     console.error('Error writing file:', err);
+        //     res.statusCode = 500;
+        //     return res.end('Internal Server Error: Could not write file.');
+        // });
+
+        return;
+  }
+})
 
 
 
