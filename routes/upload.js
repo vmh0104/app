95df5690d86062dd141d8c36070887995786d461
vmh0104 <vumanhhieuu.nd@gmail.com>
2023-11-27 18:51:01 +0700

    Your commit message

--- routes/upload.js
+++ routes/upload.js
@@ -1,12 +1,7 @@
 var express = require('express');
 var router = express.Router();
-const fs = require('fs');
-const path = require('path');
 var multer = require('multer');
 var upload = multer({ dest: __dirname });
-
-
-
 
 router.get('/upload', function(req, res, next) {
       res.render('upload', { title: 'Choose File' });
@@ -16,6 +11,7 @@ router.get('/upload', function(req, res, next) {
 router.post('/upload',upload.single('filename'), function(req, res, next) {
     console.log('Handling POST /upload');
     console.log(req.file.originalname);
+    res.end(req.file.originalname)
 });
 
 module.exports = router;
