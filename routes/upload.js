1ba67f1a7b3df8c2f1e9104b35c6b8e8aee08f24
vmh0104 <vumanhhieuu.nd@gmail.com>
2023-12-04 01:32:10 +0700

    Your commit message

--- routes/upload.js
+++ routes/upload.js
@@ -7,6 +7,8 @@ const mysql = require("mysql2");
 const csv = require('csv-parser');
 
 
+
+
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
       cb(null, __dirname);
@@ -27,22 +29,12 @@ router.post('/upload',upload.single('filename'), function(req, res, next) {
     console.log('Handling POST /upload');
     console.log(req.file.originalname);
     const fileName = req.file.originalname;
-    res.render('result', { title: 'Da upload file thanh cong:', fileName:fileName });
     
-
-
-
-
   // Bao cao ket qua
 
-
-
-
-
-
-
     const csvFilePath = __dirname + '\\' + fileName;
-    const data = [];
+    const data = []; // gia tri sai 
+    const data1 = []; // tat ca gia tri
     function isValidEmail(email) {
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         return emailRegex.test(email);
@@ -60,7 +52,11 @@ router.post('/upload',upload.single('filename'), function(req, res, next) {
         const userValue = row.user !== null && row.user !== undefined && row.user.trim() !== '' ? 'Giá trị chinh xac' : 'Giá trị là null hoặc sai định dạng';
         const mailValue = row.mail !== null && row.mail !== undefined && row.mail.trim() !== '' && isValidEmail(row.mail)  ? 'Giá trị chinh xac' : 'Giá trị là null hoặc sai định dạng';
         const bodValue = row.bod !== null && row.bod !== undefined && row.bod.trim() !== '' && isPastDate(row.bod)  ? 'Giá trị chinh xac' : 'Giá trị là null hoặc sai định dạng';
-        data.push({ ...row, id: idValue, user: userValue, mail: mailValue, bod: bodValue});
+        data1.push({ ...row, id: idValue, user: userValue, mail: mailValue, bod: bodValue});
+
+        if (idValue === 'Giá trị là null hoặc sai định dạng' || userValue === 'Giá trị là null hoặc sai định dạng' || mailValue === 'Giá trị là null hoặc sai định dạng' || bodValue === 'Giá trị là null hoặc sai định dạng') {
+          data.push({ ...row, id: idValue, user: userValue, mail: mailValue, bod: bodValue});
+        }
     })
 
     .on('end', () => {
@@ -68,13 +64,16 @@ router.post('/upload',upload.single('filename'), function(req, res, next) {
         const newCsvContent = data.map(row => Object.values(row).join(',')).join('\n');
         fs.writeFileSync(outputCsvFilePath, newCsvContent);
         console.log('New CSV file successfully created:', outputCsvFilePath);
+        const report1 = data1.length; // Tat ca
+        const report2 = data.length; // That bai
+        const report3 = data1.length - data.length; // Thanh cong
+        res.render('result', { title: 'Da upload file thanh cong: ', fileName:fileName, title2: 'Da import thanh cong file csv, voi ', report1:report1, title3: ' du lieu, ', 
+                              report3:report3, title4:' dong thanh cong, ', report2:report2, title5:' dong that bai voi nhung ly do sau: ',data });
     })
 
     .on('error', (error) => {
         console.error('Error reading CSV file:', error.message);
     });
-
-    
     const data2 = [];
     const output2CsvFilePath = __dirname + '\\' + 'data.csv';
     fs.createReadStream(csvFilePath)
@@ -95,56 +94,51 @@ router.post('/upload',upload.single('filename'), function(req, res, next) {
         const newCsvContent = data2.map(row => Object.values(row).join(',')).join('\n');
         fs.writeFileSync(output2CsvFilePath, newCsvContent);
         console.log('New CSV file successfully created:', output2CsvFilePath);
-        console.log(data2.length);
     })
 
     .on('error', (error) => {
         console.error('Error reading CSV file:', error.message);
     });
-
+    
     
 
-
-//Luu du lieu vao Db
-      const csvFilePath2 = __dirname + '\\' + 'data.csv';
-      const csvData = [];
-      let stream = fs.createReadStream(csvFilePath2);
-      let csvStream = fastcsv
-        .parse()
-        .on('data', (row) => {
-          csvData.push(row);
-        })
-        .on('end', function () {
-          const connection = mysql.createConnection({
-            host: '127.0.0.1',
-            port: '3306',
-            user: 'root',
-            password: 'Vhieu0104',
-            database: 'exam_database',
-            insecureAuth: true,
-          });
-
-          connection.connect(error => {
-            if (error) {
-              console.error('Error connecting to the database:', error);
-            } else {
-              let query = "INSERT INTO exam_info (ID, User, Mail, Bod) VALUES ?";
-              connection.query(query, [csvData], (error, response) => {
-                if (error) {
-                  console.error('Error inserting data into the database:', error);
-                } else {
-                  console.log('Data inserted successfully!');
-                }
-
-  
-              });
-            }
-          });
+// Luu du lieu vao Db
+    const csvFilePath2 = __dirname + '\\' + 'data.csv';
+    const csvData = [];
+    let stream = fs.createReadStream(csvFilePath2);
+    let csvStream = fastcsv
+      .parse()
+      .on('data', (row) => {
+        csvData.push(row);
+      })
+      .on('end', function () {
+        const connection = mysql.createConnection({
+          host: '127.0.0.1',
+          port: '3306',
+          user: 'root',
+          password: 'Vhieu0104',
+          database: 'exam_database',
+          insecureAuth: true,
         });
 
-      stream.pipe(csvStream);
-
+        connection.connect(error => {
+          if (error) {
+            console.error('Error connecting to the database:', error);
+          } else {
+            let query = "INSERT INTO exam_info (ID, User, Mail, Bod) VALUES ?";
+            connection.query(query, [csvData], (error, response) => {
+              if (error) {
+                console.error('Error inserting data into the database:', error);
+              } else {
+                console.log('Data inserted successfully!');
+              }
+            });
+          }
+        });
+      });
+    stream.pipe(csvStream);
 });
+
 
 module.exports = router;
 
